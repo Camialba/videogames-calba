72834d6b09da7e87d3b8edd5c8558792a3904e61
Camila Alba <camilabrendaalba@hotmail.com>
2022-01-19 20:31:51 -0300

    refactor client

--- api/src/clients/rawjClient.js
+++ api/src/clients/rawjClient.js
@@ -1,5 +1,51 @@
-function getAllVideoGames(){
-    //pegada a la api rawj que traiga todos los videogame /games
-    //axios para una promesa 
-    //videogames[]
+require('dotenv').config();
+const axios = require ('axios'); //importo axios
+const { get } = require('../routes');
+const {YOUR_API_KEY} = process.env; // me traigo mi apikey de .env
+
+
+const baseUrl = `https://api.rawg.io/api/$value$?key=${YOUR_API_KEY}`
+
+const getAllVideoGames = async () => {
+    const urlGet = baseUrl.replace("$value$", "games")
+    const rawjVideoGamesResponse = await getToApiRest(urlGet)
+    
+    return mapResponseFromRawj(rawjVideoGamesResponse.data)
 }
+
+const getToApiRest = async(url) =>{
+    return await axios.get(url)
+}
+
+const mapResponseFromRawj = (videoGames) =>{
+    return videoGames.map( videoGame =>{                                                                
+        const {
+            name,
+            background_image,
+            genres,
+            released,
+            rating,
+            platforms, 
+            description
+        } = videoGame 
+ 
+        return {
+            name : name,
+            background_image : background_image,
+            genres : getGenresInfo(genres),
+            released : released,
+            rating : rating,
+            platforms : getPlatformsInfo(platforms),
+            description : description
+        }
+    })
+}
+
+
+const getGenresInfo = (genres) =>{
+
+}
+
+
+
+    module.exports = {getAllVideoGames};
---
+++ api/tests/clients/rawjClient.spec.js
@@ -0,0 +1,10 @@
+const { getAllVideoGames } = require('../../src/clients/rawjClient.js');
+const { expect } = require('chai');
+
+describe('RawjClientTest', () => {
+    test('GetAllGames', () => 
+    it('first test',async () => {
+        var res = await getAllVideoGames()
+        console.log(res)
+    }))
+  });
