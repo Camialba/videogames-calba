d72e6a49479b037f7864697ff88be07e656d571b
talba <tomas.alba@mercadolibre.com>
2022-01-20 12:29:41 -0300

    add mock package add first test

--- api/src/clients/rawjClient.js
+++ api/src/clients/rawjClient.js
@@ -1,4 +1,52 @@
-function getAllVideoGames(){
-    //pegada a la api rawj que traiga todos los videogame /games
-    //axios para una promesa 
+require('dotenv').config();
+
+const axios = require ('axios');
+const YOUR_API_KEY = process.env
+const baseUrl = `https://api.rawg.io/api/$value$?key=${YOUR_API_KEY}`
+
+const getAllVideoGames = async () => {
+    const urlGet = baseUrl.replace("$value$", "games")
+    const rawjVideoGamesResponse = await getToApiRest(urlGet)
+    return mapResponseFromRawj(rawjVideoGamesResponse.data.results)
 }
+
+const getToApiRest = async(url) =>{
+    return await axios.get(url)
+}
+
+const mapResponseFromRawj = (videoGames) =>{
+    return videoGames.map( videoGame =>{                                                                
+        const {
+            id,
+            name,
+            background_image,
+            genres,
+            released,
+            rating,
+            platforms, 
+            description
+        } = videoGame 
+ 
+        return {
+            id: id,
+            name : name,
+            background_image : background_image,
+            genres : getGenresInfo(genres),
+            released : released,
+            rating : rating,
+            platforms : getPlatformsInfo(platforms),
+            description : description
+        }
+    })
+}
+
+const getGenresInfo = (genres) =>{
+    return genres.map(genre => genre.name)
+}
+
+const getPlatformsInfo = (platforms) =>{
+return platforms.map(p => p.platform.name)
+}
+    
+
+module.exports = {getAllVideoGames};
